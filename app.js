4e6cbc09c85983e29268f8468373ea4ecd7f7b0a
MiiguelHG <jesmiguelhdzg@gmail.com>
2023-06-19 20:02:06 -0600

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const port = process.env.port || 3000;
 
+//rutas personalizadas
+const rutasCategoriasAPI = require('./src/routes/categorias-routes-api');
+const rutasUsuariosAPI = require('./src/routes/usuarios-routes-api')
+const rutasAuthAPI = require('./src/routes/auth-routes-api')
 const app = express();
 
 app.set('view engine','hbs')
@@ -22,6 +26,12 @@ app.get('/login',(req,res) =>{
     res.render('signin-one')
 })
  
+//me regresa en formato JSON los datos de categorias
+app.use('/categorias/api', rutasCategoriasAPI);
+app.use('/usuarios/api', rutasUsuariosAPI);
+app.use('/auth/api', rutasAuthAPI);
+
+//me regeresa en formato HTML la vista
 app.get('/categorias',(req,res) =>{
     res.render('advance-table')
 })
